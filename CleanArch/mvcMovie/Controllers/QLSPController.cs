761a8f582f6aea9f4fcc706894edd03633af51f3
Hi am Kaito <52872325+HiamKaito@users.noreply.github.com>
2020-12-16 18:21:15 +0700

    Sản Phẩm

---
+++ CleanArch/mvcMovie/Controllers/QLSPController.cs
@@ -0,0 +1,97 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Application.DTOs;
+using mvcMovie.ViewModels;
+using Domain.Entities.Helpers;
+using Application.Interfaces;
+
+namespace Domain.Entities.Controllers
+{
+    public class QLSPController : Controller
+    {
+        private readonly IMovieService movieService;
+
+        public QLSPController(IMovieService movieService)
+        {
+            this.movieService = movieService;
+        }
+
+        public IActionResult Index(string sortOrder, string movieGenre, string searchString, int pageIndex = 1)
+        {
+            int pageSize = 5;
+            int count;
+            var movies = movieService.GetMovies(sortOrder, movieGenre, searchString, pageIndex, pageSize, out count);
+            var genres = movieService.GetGenres();
+
+            var indexVM = new IndexViewModel()
+            {
+                Movies = new PaginatedList<MovieDto>(movies, count, pageIndex, pageSize),
+                Genres = new SelectList(genres),
+                MovieGenre = movieGenre,
+                SearchString = searchString,
+                SortOrder = sortOrder
+            };
+
+            return View(indexVM);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(MovieDto movie)
+        {
+            if (ModelState.IsValid)
+            {
+                movieService.CreateMovie(movie);
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var movie = movieService.GetMovie(id);
+
+            return View(movie);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(MovieDto movie)
+        {
+            if (ModelState.IsValid)
+            {
+                movieService.UpdateMovie(movie);
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            var movie = movieService.GetMovie(id);
+
+            return View(movie);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var movie = movieService.GetMovie(id);
+
+            return View(movie);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id, bool notUsed)
+        {
+            movieService.DeleteMovie(id);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
